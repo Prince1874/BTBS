a88f206a72fdfd7b28c0418c6842666254dcf1e3
Prince1874 <pk9500291@gmail.com>
2022-05-22 21:44:54 +0530

    TitleChanged

--- BookTicket.php
+++ BookTicket.php
@@ -21,7 +21,7 @@ $result = $con->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
     <link rel="stylesheet" href="style.css"/>
-    <title>NewCustomer</title>
+    <title>Ticket</title>
 </head>
 <body>
 
